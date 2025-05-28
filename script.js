544b745192e34708d474397d2f6f45abdac2dda0
Ice <Jirateep126@gmail.com>
2025-05-28 23:18:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const texts = [
+    "Software Developer",
+    "Web Designer",
+    "UI/UX Enthusiast",
+    "Full Stack Developer"
+  ];
+  let index = 0;
+  const textSpan = document.getElementById("dynamic-text");
+
+  setInterval(() => {
+    index = (index + 1) % texts.length;
+    textSpan.textContent = texts[index];
+  }, 3000); // เปลี่ยนข้อความทุก 3 วินาที
+
+if (window.innerWidth <= 430) {
+  const toggleBtn = document.getElementById('toggleBtn');
+  const texts = document.querySelectorAll('.toggleText');
+  let expanded = false;
+
+  // เริ่มต้นซ่อน p ที่ 2 และ 3
+  for (let i = 1; i < texts.length; i++) {
+    texts[i].style.display = 'none';
+  }
+
+  toggleBtn.addEventListener('click', () => {
+    expanded = !expanded;
+
+    for (let i = 1; i < texts.length; i++) {
+      texts[i].style.display = expanded ? 'block' : 'none';
+    }
+
+    toggleBtn.textContent = expanded ? '▲ Read less' : '▼ Read more';
+  });
+}
+
+
+console.log("Hello World")
